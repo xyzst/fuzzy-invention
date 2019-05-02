4ac3954aeabb28b99e4772c80fd775589580ee29
Darren Rambaud <xyzst@users.noreply.github.com>
2019-05-01 20:48:19 -0500

    Selected values from HTML document with UIController, stored in variable in global controller

--- app.js
+++ app.js
@@ -6,19 +6,34 @@ var budgetController = (function() {})();
 /**
  * UI Controller
  */
-var UIController = (function() {})();
+var UIController = (function() {
+  return {
+    getInput: function() {
+      // Either 'inc' or 'exp', income and expense, respectively
+      var type = document.querySelector(".add__type").value;
+      var description = document.querySelector(".add__description").value;
+      var value = document.querySelector(".add__value").value;
+
+      return {
+        type: type,
+        description: description,
+        value: value
+      };
+    }
+  };
+})();
 
 /**
  * Global App Controller
  */
 var controller = (function(budgetCtrl, UICtrl) {
   var ctrlAddItem = function() {
-    // Get filled input data
+    // Grab input data ✅
+    var input = UICtrl.getInput();
     // Add item to the budget controller
     // Add the item to the UI
     // Calculate budget
     // Display the budget on the UI
-    console.log("DATA DATA");
   };
   document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
   document.addEventListener("keypress", function(event) {
