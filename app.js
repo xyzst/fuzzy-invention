20731894595281fad4cc440158143dd58d847608
Darren Rambaud <xyzst@users.noreply.github.com>
2019-05-01 20:57:00 -0500

    Abstracted UI class elements to UIController

--- app.js
+++ app.js
@@ -7,18 +7,31 @@ var budgetController = (function() {})();
  * UI Controller
  */
 var UIController = (function() {
+  var DOMStrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    inputButton: ".add__btn",
+    clickEvent: "click",
+    keyPressEvent: "keypress"
+  };
+
   return {
     getInput: function() {
       // Either 'inc' or 'exp', income and expense, respectively
-      var type = document.querySelector(".add__type").value;
-      var description = document.querySelector(".add__description").value;
-      var value = document.querySelector(".add__value").value;
+      var type = document.querySelector(DOMStrings.inputType).value;
+      var description = document.querySelector(DOMStrings.inputDescription)
+        .value;
+      var value = document.querySelector(DOMStrings.inputValue).value;
 
       return {
         type: type,
         description: description,
         value: value
       };
+    },
+    getDOMStrings: function() {
+      return DOMStrings;
     }
   };
 })();
@@ -27,6 +40,7 @@ var UIController = (function() {
  * Global App Controller
  */
 var controller = (function(budgetCtrl, UICtrl) {
+  var DOM = UICtrl.getDOMStrings();
   var ctrlAddItem = function() {
     // Grab input data ✅
     var input = UICtrl.getInput();
@@ -34,9 +48,12 @@ var controller = (function(budgetCtrl, UICtrl) {
     // Add the item to the UI
     // Calculate budget
     // Display the budget on the UI
+    console.log(input);
   };
-  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
-  document.addEventListener("keypress", function(event) {
+  document
+    .querySelector(DOM.inputButton)
+    .addEventListener(DOM.clickEvent, ctrlAddItem);
+  document.addEventListener(DOM.keyPressEvent, function(event) {
     // Older browser compatibility with which
     if (event.keyCode === 13 || event.which === 13) {
       ctrlAddItem();
