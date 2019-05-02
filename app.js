ba58ef748c461cf9363069e9ae7e27d21d45a58f
Darren Rambaud <xyzst@users.noreply.github.com>
2019-05-01 20:34:57 -0500

    Wrote scaffolding, added a couple of event listeners

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
-var budgetController = (function() {
-  var x = 23;
-  var add = function(a) {
-    return x + a;
-  };
+/**
+ * Budget Controller
+ */
+var budgetController = (function() {})();
 
-  return {
-    publicTest: function(b) {
-      // example of closure!
-      return add(b);
-    }
-  };
-})();
+/**
+ * UI Controller
+ */
+var UIController = (function() {})();
 
-var UIController = (function() {
-  return {};
-})();
-
+/**
+ * Global App Controller
+ */
 var controller = (function(budgetCtrl, UICtrl) {
-  var z = budgetCtrl.publicTest(5);
-  return {
-    anotherPublic: function() {
-      console.log(z);
-    }
+  var ctrlAddItem = function() {
+    // Get filled input data
+    // Add item to the budget controller
+    // Add the item to the UI
+    // Calculate budget
+    // Display the budget on the UI
+    console.log("DATA DATA");
   };
+  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+  document.addEventListener("keypress", function(event) {
+    // Older browser compatibility with which
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
+  });
 })(budgetController, UIController);
